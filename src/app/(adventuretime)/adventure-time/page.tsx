038a9a0b1ebda9ec155e7765520da312959eaf33
Nguyễn Văn Linh <nguyenvanlinh@MacBook-Air-cua-Nguyen-2.local>
2023-06-05 16:08:01 +0700

    fix something

--- src/app/(adventuretime)/adventure-time/page.tsx
+++ src/app/(adventuretime)/adventure-time/page.tsx
@@ -1,10 +1,6 @@
-import { Metadata } from "next";
-
 import Image from "next/image";
-import { FC, useEffect, useState } from "react";
 
 import computer from "public/giphy.gif";
-import { PrismaClient } from "@prisma/client";
 
 export const metadata = {
   title: "About Us | By Linh Nguyen Van",
@@ -12,9 +8,6 @@ export const metadata = {
 };
 
 const page = async () => {
-  const prisma = new PrismaClient();
-  const posts = await prisma.post.findMany({ where: { authorId: 3 } });
-  console.log("post", posts);
   return (
     <div className="max-w-7xl mx-auto mt-16">
       <div className="mt-5 flex flex-col md:flex-row items-center justify-between gap-x-6">
